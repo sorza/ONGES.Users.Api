708a8184cdfffe6b128244ea56acef852522a821
sorza <alexandre.sorza@outlook.com>
2026-03-18 18:13:01 -0300

    feat:padronizando classes base

---
+++ ONGES.Users.Domain/Shared/Entities/Entity.cs
@@ -0,0 +1,27 @@
+﻿namespace ONGES.Users.Domain.Shared.Entities
+{
+    public abstract class Entity(Guid id) : IEquatable<Entity>
+    {
+        public Guid Id { get; init; } = id;
+        public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; private set; }
+        public bool Equals(Entity? other)
+        {
+            if (other is null) return false;
+            return ReferenceEquals(this, other) || Id.Equals(other.Id);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == GetType() && Equals((Entity)obj);
+        }
+
+        public override int GetHashCode() => Id.GetHashCode();
+        public static bool operator ==(Entity? left, Entity? right) => Equals(left, right);
+        public static bool operator !=(Entity? left, Entity? right) => !Equals(left, right);
+
+        public void UpdateChangesDate() => UpdatedAt = DateTime.UtcNow;
+    }
+}
---
+++ ONGES.Users.Domain/Shared/ValueObjects/ValueObject.cs
@@ -0,0 +1,4 @@
+﻿namespace ONGES.Users.Domain.Shared.ValueObjects
+{
+    public abstract record ValueObject;
+}
