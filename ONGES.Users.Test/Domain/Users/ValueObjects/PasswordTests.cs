f4a67ed001dbaf46bb74a9b217a3cf768c073bb0
sorza <alexandre.sorza@outlook.com>
2026-03-18 21:01:26 -0300

    feat: Testes de dominio para Password

---
+++ ONGES.Users.Test/Domain/Users/ValueObjects/PasswordTests.cs
@@ -0,0 +1,53 @@
+﻿using ONGES.Users.Domain.Users.Exceptions.Password;
+using ONGES.Users.Domain.Users.ValueObjects;
+
+namespace ONGES.Users.Test.Domain.Users.ValueObjects
+{
+    public class PasswordTests
+    {
+        [Fact]
+        public void Create_ShouldReturnPassword_WhenValidPlainPassword()
+        {
+            // Arrange
+            var plainPassword = "Valid1!#";
+
+            // Act
+            var password = Password.Create(plainPassword);
+
+            // Assert
+            Assert.NotNull(password);
+            Assert.NotEmpty(password.Hash);
+        }
+
+        [Fact]
+        public void Create_ShouldThrowException_WhenPlainPasswordIsNullOrEmpty()
+        {
+            // Arrange
+            string plainPassword = null!;
+
+            // Act & Assert
+            Assert.Throws<NullOrEmptyPasswordException>(() => Password.Create(plainPassword));
+        }
+
+        [Fact]
+        public void Create_ShouldThrowException_WhenPlainPasswordIsTooShort()
+        {
+            // Arrange
+            var plainPassword = "short";
+
+            // Act & Assert
+            Assert.Throws<InvalidPasswordException>(() => Password.Create(plainPassword));
+        }
+
+        [Fact]
+        public void Create_ShouldThrowException_WhenPlainPasswordIsTooLong()
+        {
+            // Arrange
+            var plainPassword = new string('a', 51) + "1!#";
+
+            // Act & Assert
+            Assert.Throws<InvalidPasswordException>(() => Password.Create(plainPassword));
+        }
+
+    }
+}
